310aa4ff79b37e4c46edf31546c7c0811e1a5f6b
LuchoPersano <40143996+LuchoPersano@users.noreply.github.com>
2022-01-09 16:43:56 -0300

    v1.0.0.1
    Cambio de códio JS por mala redacción

--- main.js
+++ main.js
@@ -1,46 +1,54 @@
 console.log("Este es Lucio intentando aplicar la API de youtube para livestreams :D");
 
-  /**
+
+    /**
    * Sample JavaScript code for youtube.liveBroadcasts.insert
    * See instructions for running APIs Explorer code samples locally:
    * https://developers.google.com/explorer-help/guides/code_samples#javascript
    */
 
-   function loadClient() {
-    gapi.client.setApiKey("38164434341-tiliciovo1hrej80rr815k4bq59fi09g.apps.googleusercontent.com");
-    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-        .then(function() { console.log("GAPI client loaded for API"); },
-              function(err) { console.error("Error loading GAPI client for API", err); });
-  }
-  // Make sure the client is loaded before calling this method.
-  function execute() {
-    return gapi.client.youtube.liveBroadcasts.insert({
-      "part": [
-        "snippet,contentDetails,status"
-      ],
-      "resource": {
-        "snippet": {
-          "title": "Test broadcast"/*,
-          "scheduledStartTime": "YOUR_SCHEDULED_START_TIME",
-          "scheduledEndTime": "YOUR_SCHEDULED_END_TIME"*/
-        },
-        "contentDetails": {
-          "enableClosedCaptions": true,
-          "enableContentEncryption": true,
-          "enableDvr": true,
-          "enableEmbed": true,
-          "recordFromStart": true,
-          "startWithSlate": true
-        },
-        "status": {
-          "privacyStatus": "unlisted"
-        }
+     function authenticate() {
+        return gapi.auth2.getAuthInstance()
+            .signIn({scope: "https://www.googleapis.com/auth/youtube"})
+            .then(function() { console.log("Sign-in successful"); },
+                  function(err) { console.error("Error signing in", err); });
       }
-    })
-        .then(function(response) {
-                // Handle the results here (response.result has the parsed body).
-                console.log("Response", response);
-              },
-              function(err) { console.error("Execute error", err); });
-  }
-  gapi.load("client");
+      function loadClient() {
+        return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+            .then(function() { console.log("GAPI client loaded for API"); },
+                  function(err) { console.error("Error loading GAPI client for API", err); });
+      }
+      // Make sure the client is loaded and sign-in is complete before calling this method.
+      function execute() {
+        return gapi.client.youtube.liveBroadcasts.insert({
+          "part": [
+            "snippet,contentDetails,status"
+          ],
+          "resource": {
+            "snippet": {
+              "title": "Test broadcast",
+              "scheduledStartTime": "2022-09-01T17:15:00",
+              "scheduledEndTime": "2022-09-01T17:30:00"
+            },
+            "contentDetails": {
+              "enableClosedCaptions": true,
+              "enableContentEncryption": true,
+              "enableDvr": true,
+              "enableEmbed": true,
+              "recordFromStart": true,
+              "startWithSlate": true
+            },
+            "status": {
+              "privacyStatus": "unlisted"
+            }
+          }
+        })
+            .then(function(response) {
+                    // Handle the results here (response.result has the parsed body).
+                    console.log("Response", response);
+                  },
+                  function(err) { console.error("Execute error", err); });
+      }
+      gapi.load("client:auth2", function() {
+        gapi.auth2.init({client_id: "38164434341-tiliciovo1hrej80rr815k4bq59fi09g.apps.googleusercontent.com"});
+      });
