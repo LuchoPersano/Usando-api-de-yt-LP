b1adaecbe7ba7096768899580c8533c70251db68
LuchoPersano <40143996+LuchoPersano@users.noreply.github.com>
2022-01-09 18:48:27 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -60,44 +60,66 @@ console.log("JS v1.0.0.1 running");
    * Sample JavaScript code for youtube.liveBroadcasts.insert
    * See instructions for running APIs Explorer code samples locally:
    * https://developers.google.com/explorer-help/guides/code_samples#javascript
-    **/
+**/
 
-function loadClient() {
-  gapi.client.setApiKey("AIzaSyCLV3c-lsKKRwym6kyFsP94WSQMUVF0_w8");
-  return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-      .then(function() { console.log("GAPI client loaded for API"); },
-            function(err) { console.error("Error loading GAPI client for API", err); });
-}
-// Make sure the client is loaded before calling this method.
-function execute() {
-    var title = "Transmisión de prueba";
-  return gapi.client.youtube.liveBroadcasts.insert({
-    "part": [
-      "snippet,contentDetails,status"
-    ],
-    "resource": {
-      "snippet": {
-        "title": title,
-        "scheduledStartTime": "2022-01-10T19:00:00",
-        "scheduledEndTime": "2022-09-10T19:30:00"
-      },
-      "contentDetails": {
-        "enableClosedCaptions": true,
-        "enableContentEncryption": true,
-        "enableDvr": true,
-        "enableEmbed": true,
-        "recordFromStart": true,
-        "startWithSlate": true
-      },
-      "status": {
-        "privacyStatus": "unlisted"
+
+
+
+  /**
+   * Sample JavaScript code for youtube.liveBroadcasts.insert
+   * See instructions for running APIs Explorer code samples locally:
+   * https://developers.google.com/explorer-help/guides/code_samples#javascript
+   */
+
+  function authenticate() {
+    return gapi.auth2.getAuthInstance()
+        .signIn({scope: "https://www.googleapis.com/auth/youtube"})
+        .then(function() { console.log("Sign-in successful"); },
+              function(err) { console.error("Error signing in", err); });
+  }
+  function loadClient() {
+    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+        .then(function() { console.log("GAPI client loaded for API"); },
+              function(err) { console.error("Error loading GAPI client for API", err); });
+  }
+  // Make sure the client is loaded and sign-in is complete before calling this method.
+  function execute() {
+    return gapi.client.youtube.liveBroadcasts.insert({
+      "part": [
+        "snippet,contentDetails,status"
+      ],
+      "resource": {
+        "snippet": {
+          "title": "Test broadcast desde mi web",
+          "scheduledStartTime": "2022-01-10T8:00:00",
+          "description": "Stream de prueba :D",
+          "thumbnails": {
+            "standard": {
+              "url": "https://concepto.de/wp-content/uploads/2015/03/paisaje-e1549600034372.jpg"
+            }
+          }
+        },
+        "contentDetails": {
+          "enableClosedCaptions": true,
+          "enableContentEncryption": true,
+          "enableDvr": true,
+          "enableEmbed": true,
+          "recordFromStart": true,
+          "startWithSlate": true,
+          "enableAutoStart": true
+        },
+        "status": {
+          "privacyStatus": "unlisted",
+          "selfDeclaredMadeForKids": false
+        }
       }
-    }
-  })
-      .then(function(response) {
-              // Handle the results here (response.result has the parsed body).
-              console.log("Response", response);
-            },
-            function(err) { console.error("Execute error", err); });
-}
-gapi.load("client");
+    })
+        .then(function(response) {
+                // Handle the results here (response.result has the parsed body).
+                console.log("Response", response);
+              },
+              function(err) { console.error("Execute error", err); });
+  }
+  gapi.load("client:auth2", function() {
+    gapi.auth2.init({client_id: "38164434341-tiliciovo1hrej80rr815k4bq59fi09g.apps.googleusercontent.com"});
+  });
