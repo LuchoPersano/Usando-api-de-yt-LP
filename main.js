38fe65ae5b91af5dab2a5e18bd1f38cc504e4c11
LuchoPersano <40143996+LuchoPersano@users.noreply.github.com>
2022-01-11 15:38:52 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,97 +2,37 @@ console.log("Este es Lucio intentando aplicar la API de youtube para livestreams
 //holaaa
 console.log("JS v1.0.0.1 running");
 
+//Variables globales
+var authStatus = 0;
+var clientStatus = 0;
 
-    /**
-   * Sample JavaScript code for youtube.liveBroadcasts.insert
-   * See instructions for running APIs Explorer code samples locally:
-   * https://developers.google.com/explorer-help/guides/code_samples#javascript
-   */
-/*
-     function authenticate() {
-        return gapi.auth2.getAuthInstance()
-            .signIn({scope: "https://www.googleapis.com/auth/youtube"})
-            .then(function() { console.log("Sign-in successful"); },
-                  function(err) { console.error("Error signing in", err); });
-      }
-      function loadClient() {
-        return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-            .then(function() { console.log("GAPI client loaded for API"); },
-                  function(err) { console.error("Error loading GAPI client for API", err); });
-      }
-      // Make sure the client is loaded and sign-in is complete before calling this method.
-      function execute() {
-        return gapi.client.youtube.liveBroadcasts.insert({
-          "part": [
-            "snippet,contentDetails,status"
-          ],
-          "resource": {
-            "snippet": {
-              "title": "Test broadcast",
-              "scheduledStartTime": "2022-09-01T17:15:00",
-              "scheduledEndTime": "2022-09-01T17:30:00"
-            },
-            "contentDetails": {
-              "enableClosedCaptions": true,
-              "enableContentEncryption": true,
-              "enableDvr": true,
-              "enableEmbed": true,
-              "recordFromStart": true,
-              "startWithSlate": true
-            },
-            "status": {
-              "privacyStatus": "unlisted"
-            }
-          }
-        })
-            .then(function(response) {
-                    // Handle the results here (response.result has the parsed body).
-                    console.log("Response", response);
-                  },
-                  function(err) { console.error("Execute error", err); });
-      }
-      gapi.load("client:auth2", function() {
-        gapi.auth2.init({client_id: "38164434341-tiliciovo1hrej80rr815k4bq59fi09g.apps.googleusercontent.com"});
-      });
-      */
-
-      /**
-   * Sample JavaScript code for youtube.liveBroadcasts.insert
-   * See instructions for running APIs Explorer code samples locally:
-   * https://developers.google.com/explorer-help/guides/code_samples#javascript
-**/
-
-
-
-
-  /**
-   * Sample JavaScript code for youtube.liveBroadcasts.insert
-   * See instructions for running APIs Explorer code samples locally:
-   * https://developers.google.com/explorer-help/guides/code_samples#javascript
-   */
-
-  function authenticate() {
+function authenticate() {
     return gapi.auth2.getAuthInstance()
         .signIn({scope: "https://www.googleapis.com/auth/youtube"})
-        .then(function() { console.log("Sign-in successful"); },
-              function(err) { console.error("Error signing in", err); });
-  }
-  function loadClient() {
-    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-        .then(function() { console.log("GAPI client loaded for API"); },
-              function(err) { console.error("Error loading GAPI client for API", err); });
-  }
-  // Make sure the client is loaded and sign-in is complete before calling this method.
-  function execute() {
+        .then(function() { console.log("Sign-in successful"); authStatus = 1; },
+              function(err) { console.error("Error signing in", err); authStatus = 0; });
+}
+function loadClient() {
+  return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+      .then(function() { console.log("GAPI client loaded for API"); clientStatus = 1; },
+            function(err) { console.error("Error loading GAPI client for API", err); clientStatus = 0; });
+}
+function preExe(){
+  
+}
+// Make sure the client is loaded and sign-in is complete before calling this method.
+function execute() {
+  if(authStatus != 0 && clientStatus != 0 && titleIn != '' && descriptionIn != '' && dateIn != '' && privacyIn != 'Seleccione la privacidad de la transmisión'){
+    console.log('Iniciando petición a la API de youtube.');
     return gapi.client.youtube.liveBroadcasts.insert({
       "part": [
         "snippet,contentDetails,status"
       ],
       "resource": {
         "snippet": {
-          "title": "Test broadcast desde mi web",
-          "scheduledStartTime": "2022-01-20T8:00:00",
-          "description": "Stream de prueba :D",
+          "title": titleIn,
+          "scheduledStartTime": dateIn,
+          "description": descriptionIn,
           "thumbnails": {
             "standard": {
               "url": "https://concepto.de/wp-content/uploads/2015/03/paisaje-e1549600034372.jpg"
@@ -119,7 +59,13 @@ console.log("JS v1.0.0.1 running");
                 console.log("Response", response);
               },
               function(err) { console.error("Execute error", err); });
+  } else if(authStatus == 0 || clientStatus == 0){
+    console.log('Error: no se puede iniciar la petición porque no se ha iniciado sesión.');
+  } else {
+    console.log('Error: no se puede iniciar la petición. Rellene los campos solicitados.');
   }
-  gapi.load("client:auth2", function() {
-    gapi.auth2.init({client_id: "38164434341-tiliciovo1hrej80rr815k4bq59fi09g.apps.googleusercontent.com"});
-  });
+
+}
+gapi.load("client:auth2", function() {
+  gapi.auth2.init({client_id: "38164434341-tiliciovo1hrej80rr815k4bq59fi09g.apps.googleusercontent.com"});
+});
