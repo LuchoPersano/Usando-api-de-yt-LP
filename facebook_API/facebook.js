e9ef8fe7696e5a38e96306c483f8b98b633aa5e1
LuchoPersano <40143996+LuchoPersano@users.noreply.github.com>
2022-02-08 17:16:00 -0300

    Add files via upload

--- facebook_API/facebook.js
+++ facebook_API/facebook.js
@@ -1,11 +1,20 @@
 // Codeado por Lucio Prsano
 
-consoleLog('Este es Lucio intentando programar una emisión en directo con la API de facebook.')
+consoleLog('Este es Lucio intentando programar una emisión en directo con la API de facebook.');
+
+// Declaración de variables
+var loginBtn = document.getElementById('loginBtn');
+var loginStatus = '';
+
+// Event listeners
+loginBtn.addEventListener(onclick, login())
 
 // Verifica si la sesión está iniciada
 setTimeout(() => {
-    consoleLog('Verificando si hay alguna sesión iniciada...');
-    FB.getLoginStatus(function(response) {
-        consoleLog(response);
-    });
+
+    loginStatus = getLoginStatus();
+    if(loginStatus != 'connected'){
+        loginBtn.setAttribute('disabled', 'false')
+    }
+
 }, 2000);
