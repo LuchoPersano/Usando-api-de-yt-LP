082983253f3d2c6d0570748cf08bdc9f86882de9
LuchoPersano <40143996+LuchoPersano@users.noreply.github.com>
2022-02-09 15:30:39 -0300

    Add files via upload

--- facebook_API/funciones.js
+++ facebook_API/funciones.js
@@ -54,11 +54,13 @@ function logout(){
 }
 
 function stream(){
-    fetch('https://graph.facebook.com/v13.0/103263762285689/live_videos?planned_start_time=1644429600&status=SCHEDULED_LIVE&Este+es+el+titulo&schedule_custom_profile_image=' + imgData + '&description=EstaLaDescripcion&access_token=' + pageAccessToken, { method: 'POST' })
+    imgData = imgData.replace('data:', '');
+    fetch('https://graph.facebook.com/v13.0/103263762285689/live_videos?planned_start_time=1644429600&status=SCHEDULED_LIVE&title=Este+es+el+titulo&schedule_custom_profile_image=' + imgData + '&description=EstaLaDescripcion&access_token=' + pageAccessToken, { method: 'POST' })
         .then(response => response.json())
         .then(data => {
             consoleLog(data);
         })
+        .catch(err => consoleLog(err))
 }
 
 function getPageAccessToken(){
